d7de34b98cbcbee8d2b1d3bc0cfadc515213ddee
Yash Gajera <gajerayash2223@gmail.com>
2026-06-20 14:58:00 +0530

    Production API URL1

--- bill_system/settings.py
+++ bill_system/settings.py
@@ -21,10 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'billing.apps.BillingConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,3 +81,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'invoice_add'
 LOGOUT_REDIRECT_URL = 'login'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://embro-bill.vercel.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://embro-bill.vercel.app",
+]
